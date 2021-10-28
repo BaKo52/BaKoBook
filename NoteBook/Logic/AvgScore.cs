14335a1cd85ef2771368070fb3c1a851a0a89574
BaKo52 <74250655+BaKo52@users.noreply.github.com>
2021-10-28 11:49:45 +0200

    Commit avant moyenne module
    TP05 : je doit faire la moyenne dans la classe Module

---
+++ NoteBook/Logic/AvgScore.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Logic
+{
+    /// <summary>
+    /// Classe représentant la moyenne d'un élément pédagogique
+    /// </summary>
+    public class AvgScore
+    {
+        /// <summary>
+        /// Elément pédagogique que représente la moyenn
+        /// </summary>
+        private PedagogicalElement pe;
+        
+        /// <summary>
+        /// Variable contenant la moyenn
+        /// </summary>
+        private float average;
+
+        public float Average
+        {
+            get => average;
+            set { if (value < 0 || value > 20) { throw new ArgumentException(); } else { average = value; } }
+        }
+
+        /// <summary>
+        /// Constructeur de la classe AvgScore
+        /// </summary>
+        /// <param name="average">paramètre pour la moyenne</param>
+        /// <param name="pe">paramètre de l'élément pédagogique</param>
+        public AvgScore(float average, PedagogicalElement pe)
+        {
+            this.pe = pe ?? throw new ArgumentNullException(nameof(pe));
+            this.Average = average;
+        }
+
+        /// <summary>
+        /// Fonction ToString() de la classe
+        /// </summary>
+        /// <returns></returns>
+        public override String ToString()
+        { 
+            return pe + ": " + average;
+        }
+    }
+}
---
+++ NoteBook/TestLogic/TestAvgScore.cs
@@ -0,0 +1,66 @@
+﻿using Logic;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace TestLogic
+{
+    /// <summary>
+    /// Classe testant la classe AvgScore
+    /// </summary>
+    public class TestAvgScore
+    {
+        /// <summary>
+        /// Classe testant le constructeur de la classe AvgScore
+        /// </summary>
+        [Fact]
+        public void TestConstructeur()
+        {
+            float f = 10;
+            PedagogicalElement pe = new PedagogicalElement();
+            AvgScore avgs = new AvgScore(f, pe);
+
+            Assert.Throws<ArgumentNullException>(() => { avgs = new AvgScore(f, null); });
+
+            Assert.Equal(f, avgs.Average);
+        }
+
+        /// <summary>
+        /// méthode test les contraintes sur la variable average
+        /// </summary>
+        [Fact]
+        public void TestAverageContrainte()
+        {
+            float f = 10;
+            PedagogicalElement pe = new PedagogicalElement();
+            AvgScore avgs = new AvgScore(f, pe);
+
+            f = -1;
+            Assert.Throws<ArgumentException>(() => { avgs.Average = f; });
+
+            f = 21;
+            Assert.Throws<ArgumentException>(() => { avgs.Average = f; });
+
+            f = 10;
+            avgs.Average = f;
+        }
+
+        
+
+        /// <summary>
+        /// Méthode testant la fonction ToString de la classe AvgScore
+        /// </summary>
+        [Fact]
+        public void TestToString()
+        {
+            float f = 10;
+            Module m = new Module();
+            m.Name = "Test";
+            m.Coef = 4;
+            AvgScore avgs = new AvgScore(f, m);
+
+            Assert.Equal(m.ToString() + ": " + avgs.Average, avgs.ToString());
+        }
+    }
+}
